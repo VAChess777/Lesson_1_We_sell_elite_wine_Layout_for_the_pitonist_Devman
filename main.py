86917d3031bd7ca8d68a3eaaa0f4e99df15f2cac
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-12-08 20:24:08 +0300

    update_1_main.py

--- main.py
+++ main.py
@@ -1,43 +1,59 @@
 import datetime
-
+import os
+import pathlib
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pathlib import Path
 
+import pandas
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('vine_template.html')
-
-
-opening_winery_year = 1920
 
 
 def get_years(opening_winery_year):
     year_now = datetime.datetime.now().year
     age_delta = year_now - opening_winery_year
-    if age_delta % 10 == 1 and age_delta != 11 and age_delta % 100 != 11:  # +
-        return "{0} год".format(age_delta)
-    elif 1 < age_delta % 10 <= 4 and age_delta != 12 and age_delta != 13 and age_delta != 14:
-        return "{0} года".format(age_delta)
+    if age_delta % 10 == 1 and age_delta != 11 and age_delta % 100 != 11:
+        return f'{age_delta} год'
+    elif 1 < age_delta % 10 <= 4 and age_delta != 12 and age_delta != 13\
+            and age_delta != 14:
+        return f'{age_delta} года'
     else:
-        return "{0} лет".format(age_delta)
+        return f'{age_delta} лет'
 
 
-def main():
+def get_goods(file):
+    goods_category = pandas.read_excel(
+        file,
+        keep_default_na=False
+    ).to_dict(orient="records")
+    goods_cards = defaultdict(list)
+    for goods in goods_category:
+        goods_cards[goods["Категория"]].append(goods)
+    return goods_cards
+
+
+def render_page(excel_file, opening_winery_year):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('vine_template.html')
+    winery_age = f'Уже {get_years(opening_winery_year)} с вами:'
     rendered_page = template.render(
-        cap1_title="Проверено временем",
-        cap1_text=f'Уже {get_years(opening_winery_year)} с Вами!',
-        # cap2_title="Чёрная кепка",
-        # cap2_text="$ 120.00",
-        # cap3_title="Ещё одна чёрная кепка",
-        # cap3_text="$ 90.00",
+        winery_age=winery_age,
+        goods_cards=get_goods(excel_file)
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
+
+def main():
+    load_dotenv()
+    excel_file_name = os.getenv('EXCEL_FILE')
+    excel_file = Path(pathlib.Path.cwd(), excel_file_name)
+    opening_winery_year = int(os.getenv('OPENING_WINERY_YEAR'))
+    render_page(excel_file, opening_winery_year)
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
 
